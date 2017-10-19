9e22d06d3b8fd6c69ac279d54adafa557b7c5c1a
luispandrino <landrinomenendez@gmail.com>
2017-10-18 18:16:43 -0600

    Diccionario Funcional
    diccionario funciona con los valores dados

--- Practica#3/Diccionario.js
+++ Practica#3/Diccionario.js
@@ -6,6 +6,24 @@ function IngresarValores()
    var Valor= "";
    LLave = document.getElementById("txtLlave").value;
    Valor = document.getElementById("txtValor").value;
+   if(Valor === "false")
+   {
+       Valor = false;
+   }
+   if(Valor === "true")
+   {
+       Valor = true;
+   }
+    
+   if(isNaN(parseInt(Valor)) === false)
+   {
+       
+    Valor = parseInt(Valor);
+   }
+   if(isNaN(parseFloat(Valor))=== false)
+   {
+       Valor = parseFloat(Valor);
+   }
    diccionario.push({Key: LLave, Value: Valor});
 
 }
